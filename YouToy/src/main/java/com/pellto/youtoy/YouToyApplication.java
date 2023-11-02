91c9b6c06ac3bd46c6458c41f6fd68e245991f33
pellto <lovegkgns@gmail.com>
2023-11-02 17:06:13 +0900

    Init: init project

---
+++ YouToy/src/main/java/com/pellto/youtoy/YouToyApplication.java
@@ -0,0 +1,13 @@
+package com.pellto.youtoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouToyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YouToyApplication.class, args);
+    }
+
+}
---
+++ YouToy/src/test/java/com/pellto/youtoy/YouToyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pellto.youtoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouToyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
