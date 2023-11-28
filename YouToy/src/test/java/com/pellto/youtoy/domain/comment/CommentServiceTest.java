562039b26e085683b420c19177dce24e94c2c8f8
pellto <lovegkgns@gmail.com>
2023-11-28 16:27:57 +0900

    Feat: add comment create test

--- YouToy/src/test/java/com/pellto/youtoy/domain/comment/CommentServiceTest.java
+++ YouToy/src/test/java/com/pellto/youtoy/domain/comment/CommentServiceTest.java
@@ -1,2 +1,46 @@
-package com.pellto.youtoy.domain.comment;public class CommentServiceTest {
+package com.pellto.youtoy.domain.comment;
+
+import com.pellto.youtoy.domain.comment.dto.CreateCommentCommand;
+import com.pellto.youtoy.domain.comment.repository.CommentRepository;
+import com.pellto.youtoy.domain.comment.service.CommentWriteService;
+import com.pellto.youtoy.util.comment.CommandFixtureFactory;
+import com.pellto.youtoy.util.comment.CreateCommentCommandFixtureFactory;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+
+@Tag("domain")
+@ExtendWith(MockitoExtension.class)
+public class CommentServiceTest {
+    private static final String DOMAIN = "Comment";
+    @InjectMocks
+    private CommentWriteService commentWriteService;
+
+    @Mock
+    private CommentRepository commentRepository;
+
+    @DisplayName("[" + DOMAIN + ": create: success] comment 생성 성공 테스트")
+    @Test
+    public void createTest() {
+        var cmd = CreateCommentCommandFixtureFactory.create();
+        var comment = CommandFixtureFactory.create(cmd);
+
+        given(commentRepository.save(any())).willReturn(comment);
+
+        var createdComment = commentWriteService.create(cmd);
+
+        assertEquals(cmd.videoId(), createdComment.getVideoId());
+        assertEquals(cmd.userId(), createdComment.getUserId());
+        assertEquals(cmd.video(), createdComment.isVideo());
+        assertEquals(cmd.content(), createdComment.getContent());
+        assertEquals(cmd.repliedCommentId(), createdComment.getRepliedCommentId());
+        then(commentRepository).should(times(1)).save(any());
+    }
 }
--- YouToy/src/test/java/com/pellto/youtoy/util/comment/CreateCommentCommandFixtureFactory.java
+++ YouToy/src/test/java/com/pellto/youtoy/util/comment/CreateCommentCommandFixtureFactory.java
@@ -1,2 +1,28 @@
-package com.pellto.youtoy.util.comment;public class CreateCommentCommandFixtureFactory {
+package com.pellto.youtoy.util.comment;
+
+import com.pellto.youtoy.domain.comment.dto.CreateCommentCommand;
+
+import java.time.LocalDateTime;
+
+public class CreateCommentCommandFixtureFactory {
+    private static final Long VIDEO_ID = 1L;
+    private static final Long USER_ID = 1L;
+    private static final Long REPLIED_COMMENT_ID = 1L;
+    private static final boolean VIDEO = true;
+    private static final String CONTENT = "content";
+
+    public static CreateCommentCommand create() {
+        return create(VIDEO_ID, USER_ID, REPLIED_COMMENT_ID, VIDEO, CONTENT);
+
+    }
+
+    public static CreateCommentCommand create(
+            Long videoId,
+            Long userId,
+            Long repliedCommentId,
+            boolean video,
+            String content
+    ) {
+        return new CreateCommentCommand(videoId, userId, video, content, repliedCommentId);
+    }
 }
